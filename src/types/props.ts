baca60f6301aeaa4f1a94d1066ca63506de6108e
Cody Cahoon <codyjcahoon@gmail.com>
2020-11-19 17:52:34 -0700

    feat: Adds Props type

---
+++ src/types/props.ts
@@ -0,0 +1,13 @@
+export type Props<T extends object, Condition> = {
+  [K in keyof T]: T[K] extends Condition ? K : never;
+}[keyof T];
+
+interface Example {
+  str: string;
+  num: number;
+  boo: boolean;
+}
+
+type StringProps = Props<Example, string>; // 'str'
+type NumberProps = Props<Example, number>; // 'num'
+type BooleanProps = Props<Example, boolean>; // 'boo
